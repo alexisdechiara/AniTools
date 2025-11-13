de484bff9294af0026c866e5aefff554f1072a09
Alexis De Chiara <alexis.de-chiara@hotmail.fr>
2025-11-13 12:46:20 +0100

    Add corner-shape alternative for no supporting browsers

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,37 +1,25 @@
 <template>
-  <div
-    class="fixed max-w-4xl left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 aspect-video h-fit w-full flex flex-col p-24 bg-neutral-900 rounded-[8rem] squircle"
-  >
-    <h1 class="text-6xl font-bold text-white dark:text-neutral-300 mb-4">
-      <span class="text-primary-400 me-1">Ani</span>Tools
-    </h1>
-    <h2 class="text-2xl text-white">The awesome anime tools</h2>
-    <div class="mt-auto">
-      <UInput
-        v-model="username"
-        placeholder="Enter your username"
-        :ui="{ base: 'h-16 ps-8 rounded-full text-base', trailing: '-end-12' }"
-        @keyup.enter="login"
-      >
-        <template #trailing>
-          <UButton
-            @click="login"
-            :ui="{
-              base:
-                'size-full cursor-pointer rounded-full px-8 hover:bg-primary-400 active:bg-primary-400',
-            }"
-          >
-            <Icon
-              name="lucide:loader-circle"
-              class="size-8 animate-spin"
-              v-if="isLoading"
-            />
-            <Icon name="lucide:arrow-right" class="size-8" v-else />
-          </UButton>
-        </template>
-      </UInput>
-    </div>
-  </div>
+	<div
+		class="fixed max-w-4xl left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 aspect-video h-fit w-full flex flex-col p-24 bg-neutral-900 squircle">
+		<h1 class="text-6xl font-bold text-white dark:text-neutral-300 mb-4">
+			<span class="text-primary-400 me-1">Ani</span>Tools
+		</h1>
+		<h2 class="text-2xl text-white">The awesome anime tools</h2>
+		<div class="mt-auto">
+			<UInput v-model="username" placeholder="Enter your username"
+				:ui="{ base: 'h-16 ps-8 rounded-full text-base', trailing: '-end-12' }" @keyup.enter="login">
+				<template #trailing>
+					<UButton @click="login" :ui="{
+						base:
+							'size-full cursor-pointer rounded-full px-8 hover:bg-primary-400 active:bg-primary-400',
+					}">
+						<Icon name="lucide:loader-circle" class="size-8 animate-spin" v-if="isLoading" />
+						<Icon name="lucide:arrow-right" class="size-8" v-else />
+					</UButton>
+				</template>
+			</UInput>
+		</div>
+	</div>
 </template>
 
 <script lang="ts" setup>
@@ -39,7 +27,7 @@ import { ScoreFormat } from "#gql/default";
 const { isLoading } = useLoadingIndicator();
 
 definePageMeta({
-  layout: "none",
+	layout: "none",
 });
 
 const route = useRoute();
@@ -51,45 +39,54 @@ const { fetchStatistics } = useStatisticsStore();
 const { fetchAllAnimes } = useEntriesStore();
 
 const login = async () => {
-  if (!username.value.trim()) {
-    toast.add({
-      title: "Error",
-      description: "Please enter a username",
-      color: "error",
-      icon: "i-heroicons-exclamation-circle",
-    });
-    return;
-  }
+	if (!username.value.trim()) {
+		toast.add({
+			title: "Error",
+			description: "Please enter a username",
+			color: "error",
+			icon: "i-heroicons-exclamation-circle",
+		});
+		return;
+	}
 
-  try {
-    // Récupérer les données de l'utilisateur
-    const userData = await fetchUserData(username.value);
+	try {
+		// Récupérer les données de l'utilisateur
+		const userData = await fetchUserData(username.value);
 
-    // Utiliser l'ID de l'utilisateur pour charger les statistiques
-    await fetchStatistics(userData.id);
-    await fetchAllAnimes(
-      userData.id,
-      userData.mediaListOptions?.scoreFormat || ScoreFormat.POINT_100
-    );
+		// Utiliser l'ID de l'utilisateur pour charger les statistiques
+		await fetchStatistics(userData.id);
+		await fetchAllAnimes(
+			userData.id,
+			userData.mediaListOptions?.scoreFormat || ScoreFormat.POINT_100
+		);
 
-    // Rediriger vers la page d'accueil ou l'URL de redirection
-    const redirectTo = route.query.redirect?.toString() || "/";
-    await navigateTo(redirectTo, { replace: true });
-  } catch (error: any) {
-    console.error("Login error:", error);
+		// Rediriger vers la page d'accueil ou l'URL de redirection
+		const redirectTo = route.query.redirect?.toString() || "/";
+		await navigateTo(redirectTo, { replace: true });
+	} catch (error: any) {
+		console.error("Login error:", error);
 
-    toast.add({
-      title: "Login Error",
-      description: error.message || "An error occurred during login",
-      color: "error",
-      icon: "i-lucide-circle-alert",
-    });
-  }
+		toast.add({
+			title: "Login Error",
+			description: error.message || "An error occurred during login",
+			color: "error",
+			icon: "i-lucide-circle-alert",
+		});
+	}
 };
 </script>
 
 <style>
-.squircle {
-  corner-shape: squircle !important;
+@supports (corner-shape: squircle) {
+	.squircle {
+		corner-shape: squircle !important;
+		border-radius: 9rem;
+	}
+}
+
+@supports not (corner-shape: squircle) {
+	.squircle {
+		border-radius: 4rem !important;
+	}
 }
 </style>
